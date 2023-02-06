52cfeca20da8a86e05c128ca447c822a87e69431
hdugus-1 <hdugus-1@student.ltu.se>
2023-02-06 14:04:53 +0100

    spaceship movemnt and braking

---
+++ crumb stuff/crumb test 1/Assets/smoothcam.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smoothcam : MonoBehaviour
+{
+    // funny
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float smoothing = 0.1f;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+
+    }
+
+    private void LateUpdate()
+    {
+        smoothfollow();
+    }
+
+    public void smoothfollow()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, smoothing);
+        //transform.LookAt(target);
+    }
+
+}
--- crumb stuff/crumb test 1/Assets/spaceshipController.cs
+++ crumb stuff/crumb test 1/Assets/spaceshipController.cs
@@ -1,24 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+
+using System.Security.Cryptography;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class spaceshipController : MonoBehaviour
 {
     float vert;
     float horiz;
     float turnspeed = 75.0f;
-    float movespeed = 5;
+    float movespeed = 300.0f;
+    float maxspeed = 20.0f;
+    float brakespeed = 1000.0f;
+    float steerval;
+    float accelval;
+    float brakeval;
+    private PlayerInput playerinput;
+    private Controls playercontrols;
+    public Rigidbody rigidbody;
+
+
+    private void Awake()
+    {
+        playerinput = GetComponent<PlayerInput>();
+        playercontrols = new Controls();
+    }
+
+
+    private void OnEnable()
+    {
+        playercontrols.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playercontrols.Disable();
+    }
+
+    public void OnSteer(InputAction.CallbackContext context)
+    {
+        steerval = context.ReadValue<float>(); 
+    }
+    public void OnAccel(InputAction.CallbackContext context)
+    {
+        accelval = context.ReadValue<float>();
+    }
+    public void OnBrake(InputAction.CallbackContext context)
+    {
+        brakeval = context.ReadValue<float>();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        vert = Input.GetAxis("Vertical");
-        horiz = Input.GetAxis("Horizontal");
-        transform.Rotate(0,0 , -horiz* turnspeed * Time.deltaTime);
+        transform.Rotate(0,0, turnspeed * -steerval * Time.deltaTime);
+        rigidbody.AddForce(transform.up * accelval * Time.deltaTime * movespeed - rigidbody.velocity * brakeval * Time.deltaTime * brakespeed);
+        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxspeed);
 
-       // GetComponent.<Rigidbody>().AddForce(transform.forward * -movespeed * Time.deltaTime * vert);
-        if (Input.GetKey(KeyCode.W))
-            GetComponent<Rigidbody>().AddForce(transform.forward * movespeed * Time.deltaTime);
     }
 }
