2ce3e6e41ed560158cf3debcfca2796dc11500d8
Torrbullen <rasmus.norden@hotmail.se>
2023-02-22 18:11:24 +0100

    Menu is usable
    but i cant get input system to work

--- crumb stuff/crumb test 1/Assets/Coder/Scipt/Controls.cs
+++ crumb stuff/crumb test 1/Assets/Coder/Scipt/Controls.cs
@@ -89,6 +89,15 @@ public partial class @Controls : IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""upgradeMenuEnter"",
+                    ""type"": ""Button"",
+                    ""id"": ""8d18f2e7-4baf-431b-9ac1-450533e0cf73"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -168,6 +177,17 @@ public partial class @Controls : IInputActionCollection2, IDisposable
                     ""action"": ""gun_base_left"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""17813462-24e4-4ccb-9c92-c06388f16ac9"",
+                    ""path"": ""<Gamepad>/buttonNorth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""upgradeMenuEnter"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -183,6 +203,7 @@ public partial class @Controls : IInputActionCollection2, IDisposable
         m_Player_fire = m_Player.FindAction("fire", throwIfNotFound: true);
         m_Player_gun_base_right = m_Player.FindAction("gun_base_right", throwIfNotFound: true);
         m_Player_gun_base_left = m_Player.FindAction("gun_base_left", throwIfNotFound: true);
+        m_Player_upgradeMenuEnter = m_Player.FindAction("upgradeMenuEnter", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -249,6 +270,7 @@ public partial class @Controls : IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_fire;
     private readonly InputAction m_Player_gun_base_right;
     private readonly InputAction m_Player_gun_base_left;
+    private readonly InputAction m_Player_upgradeMenuEnter;
     public struct PlayerActions
     {
         private @Controls m_Wrapper;
@@ -260,6 +282,7 @@ public partial class @Controls : IInputActionCollection2, IDisposable
         public InputAction @fire => m_Wrapper.m_Player_fire;
         public InputAction @gun_base_right => m_Wrapper.m_Player_gun_base_right;
         public InputAction @gun_base_left => m_Wrapper.m_Player_gun_base_left;
+        public InputAction @upgradeMenuEnter => m_Wrapper.m_Player_upgradeMenuEnter;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -290,6 +313,9 @@ public partial class @Controls : IInputActionCollection2, IDisposable
                 @gun_base_left.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnGun_base_left;
                 @gun_base_left.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnGun_base_left;
                 @gun_base_left.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnGun_base_left;
+                @upgradeMenuEnter.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnUpgradeMenuEnter;
+                @upgradeMenuEnter.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnUpgradeMenuEnter;
+                @upgradeMenuEnter.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnUpgradeMenuEnter;
             }
             m_Wrapper.m_PlayerActionsCallbackInterface = instance;
             if (instance != null)
@@ -315,6 +341,9 @@ public partial class @Controls : IInputActionCollection2, IDisposable
                 @gun_base_left.started += instance.OnGun_base_left;
                 @gun_base_left.performed += instance.OnGun_base_left;
                 @gun_base_left.canceled += instance.OnGun_base_left;
+                @upgradeMenuEnter.started += instance.OnUpgradeMenuEnter;
+                @upgradeMenuEnter.performed += instance.OnUpgradeMenuEnter;
+                @upgradeMenuEnter.canceled += instance.OnUpgradeMenuEnter;
             }
         }
     }
@@ -328,5 +357,6 @@ public partial class @Controls : IInputActionCollection2, IDisposable
         void OnFire(InputAction.CallbackContext context);
         void OnGun_base_right(InputAction.CallbackContext context);
         void OnGun_base_left(InputAction.CallbackContext context);
+        void OnUpgradeMenuEnter(InputAction.CallbackContext context);
     }
 }
---
+++ crumb stuff/crumb test 1/Assets/upgradeTrigger.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+
+public class upgradeTrigger : MonoBehaviour
+{
+    public Transform spaceshipTransform;
+    public bool spaceshipInbound;
+    public GameObject upgradePanel;
+    public Controls controls;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Spaceship"))
+        {
+            spaceshipInbound = true;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Spaceship"))
+        {
+            spaceshipInbound = false;
+        }
+
+    }
+
+    void Awake()
+    {
+        controls = new Controls();
+    }
+
+    void Start()
+    {
+        spaceshipTransform = GameObject.FindGameObjectWithTag("Spaceship").transform;
+        upgradePanel = GameObject.FindGameObjectWithTag("UpgradePanel");
+    }
+
+    void Update()
+    {  
+        upgradePanel.SetActive(spaceshipInbound);
+        if (spaceshipInbound)
+        {
+            /*
+            if (Input.GetPressDown("u"))
+            {
+                SceneManager.LoadScene("upgradeMenu");
+            }
+            */
+            controls.performed += ctx => SceneManager.LoadScene("upgradeMenu");
+        }
+    }
+    
+    void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Disable();
+    }
+
+}
