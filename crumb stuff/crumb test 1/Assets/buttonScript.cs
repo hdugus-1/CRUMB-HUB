7b48acb5703c7fc541064711e5a21c1bc5d54d46
Torrbullen <rasmus.norden@hotmail.se>
2023-02-27 14:42:05 +0100

    error import assets

---
+++ crumb stuff/crumb test 1/Assets/buttonScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonScript : MonoBehaviour
+{
+    [SerializeField] private string Scene = "Zone";
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void SettingsGameButton()
+    {
+        SceneManager.LoadScene("UI_controlselection");
+    }
+
+    public void ExitGameButton()
+    {
+        Application.Quit();
+    }
+}
